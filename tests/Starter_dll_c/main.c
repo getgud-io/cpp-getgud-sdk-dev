9b963c0c946f94bf96d73ee9ec3123a3aabe6719
leetthewire <madmoloh@icloud.com>
2023-06-06 18:12:13 +0800

    tests folder updated

--- tests/Starter_dll_c/main.c
+++ tests/Starter_dll_c/main.c
@@ -1,11 +1,13 @@
-#include "../../include/GetGudSdk_C.h"
+#include <GetGudSdk_C.h>
+#ifdef _WIN32
 #include <Windows.h>
 #include <stdio.h>
+#endif
 
-int main()
-{
+int main() {
   init();
-  char gameGuid[36];
+  //Fill the Game info
+  char gameGuid[37] = { 0 }; //guid + \0
   int gameGuidSize = 0;
   struct StartGameInfo gameInfo;
   gameInfo.gameMode = "gameMode";
@@ -15,27 +17,40 @@ int main()
   gameInfo.serverGuid = "serverGuid";
   gameInfo.serverGuidSize = 10;
   gameInfo.titleId = 1;
+  //Start the Game
   gameGuidSize = StartGame(gameInfo, gameGuid);
 
-  char matchGuid[36];
+  //Fill the Match info
+  char matchGuid[37] = { 0 }; //guid + \0
   int matchGuidSize = 0;
   struct StartMatchInfo matchInfo;
-  /*
-  *   char* gameGuid;
-  int gameGuidSize;
-  char* matchMode;
-  int matchModeSize;
-  char* mapName;
-  int mapNameSize;
-  */
   matchInfo.gameGuid = gameGuid;
   matchInfo.gameGuidSize = 36;
   matchInfo.matchMode = "4143253";
   matchInfo.matchModeSize = 7;
   matchInfo.mapName = "serverGuid";
   matchInfo.mapNameSize = 10;
+  //Start the Match
   matchGuidSize = StartMatch(matchInfo, matchGuid);
 
+  struct BaseActionData baseData;
+  baseData.actionTimeEpoch = 1684059337532;
+  baseData.matchGuid = matchGuid;
+  baseData.matchGuidSize = 36;
+  baseData.playerGuid = "player_guid";
+  baseData.playerGuidSize = 11;
 
-  Sleep(2000);
+  SendAttackAction(baseData, "W_G", 3);
+
+  SendDeathAction(baseData);
+
+  MarkEndGame(gameGuid, 36);
+
+  Sleep(6000);
+
+  dispose();
+
+  system("pause");
+
+  return 0;
 }
